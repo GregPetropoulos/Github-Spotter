5aae32b34c5cd61624e09b5efd64df572f9a4166
GP <gregpetropoulos@yahoo.com>
2022-02-24 07:39:51 -0500

    Building out the shell of the application, such as not found page, about page, footer components. i added react icons and the react-router-dom v6 to link pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/layouts/Footer';
+import Home from './pages/Home';
+import Navbar from './components/layouts/Navbar';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      {/* <Routes> */}
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+          Content
+        </main>
+        <Footer />
+      </div>
+
+      {/* </Routes> */}
+    </Router>
   );
 }
 
