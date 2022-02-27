d427d32083d0069538986adbc75ba803ccdace83
GP <gregpetropoulos@yahoo.com>
2022-02-27 14:33:23 -0500

    Built out the UserItem to show users via login and image.

--- client/src/components/users/UserResults.js
+++ client/src/components/users/UserResults.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Spinner } from '../layouts/Spinner';
+import UserItem from './UserItem';
+
 const UserResults = () => {
   const [users, setUsers] = useState([]);
   // Always use true by default to avoid less code
@@ -13,30 +15,26 @@ const UserResults = () => {
   const fetchUsers = async () => {
     const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
       headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+      }
     });
     const data = await response.json();
     setUsers(data);
     setLoading(false);
     console.log(data);
-};
-// console.log('users:', users);
-if(!loading){
-
-
-
-
-    return(
-    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-      {users.map((user) => (
-          <h3>{user.login}</h3>
-      ))}
-    </div>
-    )
-}else{
-   return  <Spinner/>
-}
+  };
+  // console.log('users:', users);
+  if (!loading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+<UserItem key={user.id} user={user}/>
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 };
 
 export default UserResults;
