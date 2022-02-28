f7a7a87674d3d2a562786e3c31dd1840969b635a
GP <gregpetropoulos@yahoo.com>
2022-02-28 06:08:38 -0500

    Cleaned up the fetchUsers for testing purposes, removed the useEffect, Changed the initialState loading to false and created a SET_LOADING action object to handle the loading true.

--- client/src/context/github/GithubContext.js
+++ client/src/context/github/GithubContext.js
@@ -1,47 +1,51 @@
 import { createContext, useReducer } from 'react';
 import githubReducer from './GithubReducer';
 
-
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
+  //* INITIALSTATE SET TO AN OBJECT
+  const intitialState = {
+    users: [],
+    loading: false
+  };
+  //*USEREDUCER HOOK
+  const [state, dispatch] = useReducer(githubReducer, intitialState);
 
-//* INITIALSTATE SET TO AN OBJECT
-    const intitialState={
-        users:[],
-        // Always use true by default to avoid less code
-        loading:true
-    }
-    //*USEREDUCER HOOK 
-    const [state, dispatch]= useReducer(githubReducer, intitialState)
-
-    // INITIALSTATE AND USEREDUCER REPLACE THE USESTATE
-//   const [users, setUsers] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-  // async function fetch request
+  //* ASYNC FUNCTION TO FETCH REQUEST TO GET USER DATA-TESTING PURPOSES
   const fetchUsers = async () => {
+    //   set loading sets to true via reducer and the GET_USERS sets it back to false
+    setLoading();
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`
       }
     });
     const data = await response.json();
-   dispatch({
-       type:'GET_USERS',
-       payload:data,
-   })
+    dispatch({
+      type: 'GET_USERS',
+      payload: data
+    });
   };
+
+  // * SET LOADING
+  const setLoading = () =>
+    dispatch({
+      type: 'SET_LOADING'
+    });
+
   return (
     <GithubContext.Provider
       value={{
-        users:state.users,
-        loading:state.loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers
-      }}>{children}</GithubContext.Provider>
+      }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
-export default GithubContext
+export default GithubContext;
